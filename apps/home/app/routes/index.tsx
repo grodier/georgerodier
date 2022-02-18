c9b8d696e9fe75929188edd1cb29c5643ddfa6ee
grodier <grodier@gmail.com>
2022-02-17 20:34:13 -0500

    Added upcoming sections

--- apps/home/app/routes/index.tsx
+++ apps/home/app/routes/index.tsx
@@ -1,8 +1,13 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <main>
       <h1>George Rodier</h1>
-      <p>Have to wait for now, but cool things coming soon!</p>
-    </div>
+      <section>
+        <h2>Selected Writing</h2>
+      </section>
+      <section>
+        <h2>Selected Projects</h2>
+      </section>
+    </main>
   );
 }
